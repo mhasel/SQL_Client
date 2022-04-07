11d2d96c9a79637cd62b1ace26c29a069204b727
Michael <78988079+mhasel@users.noreply.github.com>
2022-04-07 15:56:47 +0200

    Delete queries directory

--- queries/welche-fische-gibt-es-und-wer-hat-diese-gefangen.sql
+++
@@ -1,7 +0,0 @@
--- welche fische gibt es und wer hat diese schon gefangen
-SELECT
-	tfish.name, tcatch.fisher
-FROM
-	tfish
-		LEFT JOIN
-	tcatch ON tcatch.fish = tfish.id
--- queries/wer-hat-schon-einen-XY-gefangen.sql
+++
@@ -1,9 +0,0 @@
--- wer hat schonmal einen XY gefangen
-
-SELECT
-	CONCAT(tfisher.name, ' ', tfisher.surname) AS 'NAME',
-    tfish.name as 'FISH'
-FROM
-	tfish, tfisher, tcatch
-WHERE tcatch.fish = tfish.id AND tcatch.fisher = tfisher.id  AND tfish.name = 'karp'
-	
